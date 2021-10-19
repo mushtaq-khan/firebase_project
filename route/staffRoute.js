fb6a8b316cd00344ba0f92d31e5aa28943b60def
zedaan <zedanrumi@gmail.com>
2021-10-19 15:53:38 +0500

    testing

--- route/staffRoute.js
+++ route/staffRoute.js
@@ -1,7 +1,11 @@
 const express = require('express');
-const { addStaff, getAllStaffDetails, getSingleUser } = require('../staffController/staffController');
+const { addStaff, getAllStaffDetails, getSingleUser, updateStaffDetails, updateAllStaffDetails, deleteStaffDetails, deleteAllStaffDetails } = require('../staffController/staffController');
 const route = express.Router();
 route.post('/post', addStaff);
 route.get('/getAll',getAllStaffDetails);
 route.get('/get:id',getSingleUser);
+route.put('/update:id',updateStaffDetails);
+route.put('/update',updateAllStaffDetails);
+route.delete('/delete/:id',deleteStaffDetails)
+route.delete('/delete',deleteAllStaffDetails)
 module.exports = route;
--- staffController/staffController.js
+++ staffController/staffController.js
@@ -1,75 +1,76 @@
 // https://www.youtube.com/watch?v=TLdIqIfJmko
-const Staff = require('../config/config');
-const Staffs = require('../models/staff');
+const Staff = require("../config/config");
+const Staffs = require("../models/staff");
 // const uuid = require('uuid');
 // const firebase = require('firebase-admin');
 
-const addStaff = async function(req,res){
-  try{
+const addStaff = async function (req, res) {
+  try {
     let data = req.body;
-    data.id=Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    console.log(data)
+    data.id =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
+    // console.log(data)
     const result = await Staff.add(data);
-    res.status(200).json({result,message:"data saved sexsexfully"});
-  }catch(err){
-    res.status(404).json('something went wrong');
+    res.status(200).json({ result, message: "data saved sexsexfully" });
+  } catch (err) {
+    res.status(404).json("something went wrong");
   }
-}
+};
 
-const getAllStaffDetails = async function(req,res){
-  try{
+const getAllStaffDetails = async function (req, res) {
+  try {
     //querySnapshot
     const data = await Staff.get();
     //documentSnapshot
     const result = data.docs;
-    const staffArray = []
-    if(result.empty){
-      res.status(404).json('data not found');
-    }else{
-      result.forEach(function(items){
-        console.log('itemsss',items)
+    const staffArray = [];
+    if (result.empty) {
+      res.status(404).json("data not found");
+    } else {
+      result.forEach(function (items) {
+        console.log("itemsss", items);
         const staff = new Staffs(
           items.data().name,
           items.data().age,
           items.data().country,
-          items.data().email,    
+          items.data().email,
           items.data().id
-        )
+        );
         staffArray.push(staff);
-      })
+      });
       res.status(200).json({
-        message:"data reterived sucessfully",
-        staffArray
+        message: "data reterived sucessfully",
+        staffArray,
       });
     }
-  }catch(err){
-    res.status(400).json(err)
+  } catch (err) {
+    res.status(400).json(err);
   }
-}
+};
 
-const getSingleUser =async function(req,res){
-// const id = req.params.id
-const { id} = req.params
+const getSingleUser = async function (req, res) {
+  // const id = req.params.id
+  const { id } = req.params;
 
-console.log("id",id)
-console.log(typeof(id))
-// const id = '3VG8pDwVRezdbDVfcwb5'
-const data = await Staff.doc(id).get();
-// const data = await Staff.get();
-console.log('data',data);
+  console.log("id", id);
+  console.log(typeof id);
+  // const id = '3VG8pDwVRezdbDVfcwb5'
+  const data = await Staff.doc(id).get();
+  // const data = await Staff.get();
+  console.log("data", data);
 
-console.log(data.empty)
-if(!data.exists){
-  res.status(404).json('data not found');
-}else{
-  console.log('test')
+  console.log(data.empty);
+  if (!data.exists) {
+    res.status(404).json("data not found");
+  } else {
+    console.log("test");
 
-  res.status(200).json(data.data());
-  
+    res.status(200).json(data.data());
 
-  // res.json(data.data());
-  // res.json(data.docs)
-}
+    // res.json(data.data());
+    // res.json(data.docs)
+  }
 
   // const { id } = req.params;
   // console.log("id",id)
@@ -82,10 +83,44 @@ if(!data.exists){
   // }else{
   //   res.json(result);
   // }
+};
+
+const updateStaffDetails = async function (req, res) {
+  const { id } = req.params;
+  let test = req.body;
+  console.log("test", test);
+  const data = await Staff.doc(id);
+  await data.update(test);
+  // console.log('data',data)
+  // res.json(result);
+  res.json("data sucessfully updated");
+};
+
+const deleteStaffDetails = async function (req, res) {
+  try {
+    const { id } = req.params;
+    const data = await Staff.doc(id).delete();
+    res.status(200).json("record deleted sucessfully");
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+
+const deleteAllStaffDetails = async function(req,res){
+  const data = await Staff.get();
+  const result = await data.docs;
+  const test = result.delete()
+  res.json(test);
 }
+
+const updateAllStaffDetails = async function (req, res) {};
 
 module.exports = {
   addStaff,
   getAllStaffDetails,
-  getSingleUser
-}
+  getSingleUser,
+  updateStaffDetails,
+  updateAllStaffDetails,
+  deleteStaffDetails,
+  deleteAllStaffDetails
+};
